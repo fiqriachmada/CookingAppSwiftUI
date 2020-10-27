4c02c9d907ae2c1d9cf3cc981ba534b56e91b9d7
fiqriachmada <achmada1998@gmail.com>
2020-10-27 09:55:18 +0700

    Initial Commit

---
+++ CookingApp/CookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookingAppApp.swift
+//  CookingApp
+//
+//  Created by Achmada Fiqri A Rasyad on 27/10/20.
+//
+
+import SwiftUI
+
+@main
+struct CookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
